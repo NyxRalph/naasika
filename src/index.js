af2a3656c316755f6436cdb1d7ce95657b221697
Ralph Andy Menz <andyralphmenz@gmail.com>
2025-03-03 21:06:44 +0000

    ...

--- src/index.js
+++ src/index.js
@@ -23,12 +23,4 @@
 //     });
 //   });
 
-document.addEventListener("scroll", () => {
-  const sections = document.querySelectorAll(".parallax");
-console.log(sections)
-  sections.forEach((section) => {
-    const scrollSpeed = 0.15;
-    const offset = window.scrollY * scrollSpeed;
-    section.style.backgroundPositionY = -`${offset}px`;
-  });
-});
+
