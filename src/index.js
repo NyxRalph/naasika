f4d9d3e99199e02b32902c1ae0d7f06fb7add1e7
Ralph Andy Menz <andyralphmenz@gmail.com>
2025-03-03 14:52:25 +0000

    ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-window.addEventListener("scroll", function () {
-    let scrolled = window.scrollY;
+// window.addEventListener("scroll", function () {
+//     let scrolled = window.scrollY;
 
-    document.querySelectorAll(".parallax").forEach((section, index) => {
-        let speed = 0.3 + index * 0.05; 
-        section.style.backgroundPositionY = -(scrolled * speed) + "px";
-    });
+//     document.querySelectorAll(".parallax").forEach((section, index) => {
+//         // console.log(index)
+//         let speed = 0.05 + index * 0.1;
+//         section.style.backgroundPositionY = -(scrolled * speed) + "px";
+//     });
+// });
+
+// document.addEventListener('scroll', () => {
+//     const sections = document.querySelectorAll('.parallax');
+
+//     sections.forEach(section => {
+//       const rect = section.getBoundingClientRect();
+//       const isInView = rect.top <= window.innerHeight && rect.bottom >= 0;
+
+//       if (isInView) {
+//         const scrollSpeed = 0.15;
+//         const offset = window.scrollY * scrollSpeed;
+//         section.style.backgroundPositionY = -`${offset}px`;
+//       }
+//     });
+//   });
+
+document.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll(".parallax");
+console.log(sections)
+  sections.forEach((section) => {
+    const scrollSpeed = 0.15;
+    const offset = window.scrollY * scrollSpeed;
+    section.style.backgroundPositionY = -`${offset}px`;
+  });
 });
